7c5038b610763a31abfe2b872a3e432c58aa1352
Veni92 <116999531+Veni92@users.noreply.github.com>
2023-04-25 17:56:41 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,8 @@
-echo "Building docker image"
-docker build -t IMAGENAME:IMAGETAG .
+#!/bin/bash
+sudo docker stop $(docker ps -a -q)
+sudo docker rm $(docker ps -a -q)
+sudo docker image rmi -f veni9297/prod:react-app
+sudo docker image rmi -f veni9297/dev:react-app
+sudo docker rmi -f react-app:latest
+sudo  docker rmi -f node:latest
+sudo docker build -t react-app:latest .
